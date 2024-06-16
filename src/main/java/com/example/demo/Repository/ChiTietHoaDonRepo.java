486739255a4c847095bb45cc6c071979fb3c4f0c
Khoa <khoaptdps34956@fpt.edu.vn>
2024-06-16 15:53:29 +0700

    user

--- src/main/java/com/example/demo/Repository/ChiTietHoaDonRepo.java
+++ src/main/java/com/example/demo/Repository/ChiTietHoaDonRepo.java
@@ -3,7 +3,9 @@ package com.example.demo.Repository;
 import com.example.demo.Entity.ChiTietHoaDon;
 import com.example.demo.Entity.HoaDon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChiTietHoaDonRepo extends JpaRepository<ChiTietHoaDon,Integer> {
 
 
--- src/main/java/com/example/demo/Repository/HoaDonRepo.java
+++ src/main/java/com/example/demo/Repository/HoaDonRepo.java
@@ -2,7 +2,9 @@ package com.example.demo.Repository;
 
 import com.example.demo.Entity.HoaDon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HoaDonRepo extends JpaRepository<HoaDon,Integer> {
 
 
