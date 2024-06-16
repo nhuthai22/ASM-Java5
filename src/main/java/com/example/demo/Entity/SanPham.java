9d58ed4d37d8d256126fe0cb65c78dbdd6f5bac0
nhuthai22 <151977282+nhuthai22@users.noreply.github.com>
2024-06-16 13:54:05 +0700

    full code

--- src/main/java/com/example/demo/Entity/SanPham.java
+++ src/main/java/com/example/demo/Entity/SanPham.java
@@ -23,11 +23,63 @@ public class SanPham {
     @JoinColumn(name = "LSPID")
     @JsonIgnore
     private LoaiSanPham loaiSanPham;
-
+    
+    @Column(name="TenSP")
     private String TenSP;
+    
+    @Column(name="Gia")
     private Double Gia;
-    private Integer quantity;
+    
+    @Column(name="quantity")
+    private String quantity;
+    
+    @Column(name="mo_ta")
     private String mo_ta;
+    
+    @Column(name="images")
     private String images;
-
+    
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LoaiSanPham getLoaiSanPham() {
+		return loaiSanPham;
+	}
+	public void setLoaiSanPham(LoaiSanPham loaiSanPham) {
+		this.loaiSanPham = loaiSanPham;
+	}
+	public String getTenSP() {
+		return TenSP;
+	}
+	public void setTenSP(String tenSP) {
+		TenSP = tenSP;
+	}
+	public Double getGia() {
+		return Gia;
+	}
+	public void setGia(Double gia) {
+		Gia = gia;
+	}
+	public String getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	public String getMo_ta() {
+		return mo_ta;
+	}
+	public void setMo_ta(String mo_ta) {
+		this.mo_ta = mo_ta;
+	}
+	public String getImages() {
+		return images;
+	}
+	public void setImages(String images) {
+		this.images = images;
+	}
+    
 }
